0b51c6ed9262cfab96b7c9bc0ae6b4f9a3e3c380
Rodrigo Vargas Sainz <rodrigosj@gmail.com>
2022-02-27 00:43:54 -0400

    FINALIZACION

--- 2_1_Basics of Evaluating Machine Learning Algorithms Caret.R
+++ 2_1_Basics of Evaluating Machine Learning Algorithms Caret.R
@@ -50,3 +50,37 @@ y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>%
 y_hat <- factor(y_hat)
 mean(y_hat == test_set$sex)
 
+# tabulate each combination of prediction and actual value
+table(predicted = y_hat, actual = test_set$sex)
+test_set %>% 
+  mutate(y_hat = y_hat) %>%
+  group_by(sex) %>% 
+  summarize(accuracy = mean(y_hat == sex))
+prev <- mean(y == "Male")
+
+confusionMatrix(data = y_hat, reference = test_set$sex)
+
+# maximize F-score
+cutoff <- seq(61, 70)
+F_1 <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  F_meas(data = y_hat, reference = factor(train_set$sex))
+})
+
+data.frame(cutoff, F_1) %>% 
+  ggplot(aes(cutoff, F_1)) + 
+  geom_point() + 
+  geom_line()
+
+max(F_1)
+
+best_cutoff <- cutoff[which.max(F_1)]
+best_cutoff
+
+y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+sensitivity(data = y_hat, reference = test_set$sex)
+specificity(data = y_hat, reference = test_set$sex)
+
+#PRUEBA DE FINALIZACIÓN DE CODIGO
