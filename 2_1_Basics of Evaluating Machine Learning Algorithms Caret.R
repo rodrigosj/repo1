8a6e201cab6f4780751db177ce3fc8229ee92d29
Rodrigo Vargas Sainz <rodrigosj@gmail.com>
2022-02-27 00:33:03 -0400

    ANADIENDO COMENTARIOS

--- 2_1_Basics of Evaluating Machine Learning Algorithms Caret.R
+++ 2_1_Basics of Evaluating Machine Learning Algorithms Caret.R
@@ -3,27 +3,33 @@ library(caret)
 library(dslabs)
 data("heights")
 # define the outcome and predictors
+# DEFINIENDO LA SALIDA Y LOS PREDICTORES
 y <- heights$sex
 x <- heights$height
 
 # generate training and test sets
+# GENERANDO EL SET DE ENTRENAMIENTO Y DE TEST
 set.seed(2, sample.kind = "Rounding") # if using R 3.5 or earlier, remove the sample.kind argument
 test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
 test_set <- heights[test_index, ]
 train_set <- heights[-test_index, ]
 
 # guess the outcome
+# ADIVINANDO LA SALIDA
+
 y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE)
 y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% 
   factor(levels = levels(test_set$sex))
 
 # compute accuracy
+# CALCULANDO LA PRESICIÓN
 mean(y_hat == test_set$sex)
 heights %>% group_by(sex) %>% summarize(mean(height), sd(height))
 y_hat <- ifelse(x > 62, "Male", "Female") %>% factor(levels = levels(test_set$sex))
 mean(y == y_hat)
 
 # examine the accuracy of 10 cutoffs
+# EXAMINANDO LA PRESICIÓN DE 10 CORTES
 cutoff <- seq(61, 70)
 accuracy <- map_dbl(cutoff, function(x){
   y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
@@ -43,3 +49,4 @@ y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>%
   factor(levels = levels(test_set$sex))
 y_hat <- factor(y_hat)
 mean(y_hat == test_set$sex)
+
