1c9e636c358187aa21c26836ff891a2948dbce6a
Rodrigo Vargas Sainz <rodrigosj@gmail.com>
2022-03-01 10:47:10 -0400

    hasta pagina 690

--- MovieLens.R
+++ MovieLens.R
@@ -356,3 +356,54 @@ lambda <- lambdas[which.min(rmses)]
 lambda
 
 #ME QUEDE EN 33.11 PAGINA 688
+
+#FACTORIZACIÓN DE MATRICES
+#solo se cogerá a las peliculas que tienen hartas evaluaciones y a usuarios que hicieron hartas evaluaciones
+train_small <- movielens %>%
+  group_by(movieId) %>%
+  filter(n() >=50 | movieId == 3252) %>%
+  ungroup() %>%
+  group_by(userId) %>%
+  filter(n() >= 50) %>%
+  ungroup()
+
+y <- train_small %>%
+  select(userId, movieId, rating) %>%
+  pivot_wider(names_from = "movieId", values_from = "rating") %>%
+  as.matrix()
+#añadir nombre de fila y columna
+rownames(y) <- y[,1]
+y <- y[,-1]
+
+movie_titles <- movielens %>%
+  select(movieId, title) %>%
+  distinct()
+
+colnames(y) <- with(movie_titles,  title[match(colnames(y), movieId)])
+
+#convertimos en residus eliminando los efectos de columna y fila
+
+y <- sweep(y, 2, colMeans(y, na.rm = TRUE))
+y <- sweep(y, 1, rowMeans(y, na.rm = TRUE))
+
+m_1 <- "Godfather, The"
+m_2 <- "Godfather: Part II, The"
+p1 <- qplot(y[ ,m_1], y[,m_2], xlab = m_1, ylab = m_2)
+
+m_3 <- "Goodfellas"
+p2 <- qplot(y[ ,m_1], y[,m_3], xlab = m_1, ylab = m_3)
+
+m_4 <- "You've Got Mail"
+m_5 <- "Sleepless in Seattle"
+p3 <- qplot(y[,m_4], y[,m_5], xlab = m_4, ylab = m_5)
+
+gridExtra::grid.arrange(p1,p2,p3, ncol=3)
+
+#acortando nombres de las pelis
+x <- y[, c(m_1, m_2, m_3, m_4, m_5)]
+short_names <- c("Godfather", "Godfather2", "Goodfellas",
+                 "You've Got", "Sleepless")
+colnames(x) <- short_names
+cor(x, use = "pairwise.complete")
+
+round(r,1)
